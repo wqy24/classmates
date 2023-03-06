5c24d198463bb0b5a4a9d01c5a6711b24bbeaebb
Woo, Chyo（伍庆耀） <as2734@126.com>
2023-03-06 21:01:11 +0800

    Make loadInfo script

--- loadInfo.js
+++ loadInfo.js
@@ -2,3 +2,12 @@ var octokit = new Octokit({
   auth: "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"
 });
 
+$("input[type=submit]").on("click", event => {
+  octokit.repos.creatOrUpdateFile({
+    owner: "wqy24",
+    repo: "classmates",
+    branch: "gh-pages",
+    path:"index.html",
+    message: "Add classmate info"
+  });
+})
