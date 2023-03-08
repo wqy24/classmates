303f40e87589370bf8ee37c6220d3c75f6e2ac68
Woo, Chyo（伍庆耀） <as2734@126.com>
2023-03-08 21:30:26 +0800

    Change template

--- loadInfo.js
+++ loadInfo.js
@@ -5,26 +5,20 @@ var octokit = new Octokit({
 var template = 
   `
   <style>
-    #stu{no} th, #stu{no} pre {
+    #stu{no} li, #stu{no} pre {
       display: block;
+      margin: 5px
       border: 1px solid #000;
+      background-color: {color}
       color: {textcolor};
     }
   </style><div id="stu{no}">
-  <table>
-    <tr>
-      <th>姓名</th>
-      <th>学号</th>
-      <th>性别</th>
-      <th>E-mail</th>
-    </tr>
-    <tr>
-      <td>{name}<td>
-      <td>{no}</td>
-      <td>{sex}</td>
-      <td>{mail}</td>
-    </tr>
-  </table>
+  <ul>
+    <li>姓名：{name}</li>
+    <li>学号：{no}</li>
+    <li>性别：{sex}</li>
+    <li>E-mail：{mail}</li>
+  </ul>
   <p>想说的话：</p>
   <pre>{wannasay}</pre></div>`;
 
@@ -33,9 +27,10 @@ $("input[type=submit]").on("click", event => {
     owner: "wqy24",
     repo: "classmates",
     branch: "gh-pages",
-    path:"index.html",
-    file_sha: (await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {owner: 'OWNER',repo: 'REPO',path: 'PATH',headers: {'X-GitHub-Api-Version': '2022-11-28'}})).sha,
-    message: "Add classmate info"
+    path:"index.html
+    file_sha: (await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {owner: 'wqy24',repo: 'classmates',path: 'index.html',headers: {'X-GitHub-Api-Version': '2022-11-28'}})).sha,
+    message: "Add classmate info",
+    content: await getContent() + Base64.encode(getStr());
   });
   return event.preventDefault ? event.preventDefault() : false;
 });
